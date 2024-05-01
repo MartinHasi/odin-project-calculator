0f9592ca39b74bfe3079a632e7031731d8e79c6d
MartinHasi <martinhasieber@gmail.com>
2024-05-01 07:16:52 +0200

    updated index.html added buttons + classes
    updated script.js
    startet by 0 with logic

--- js/script.js
+++ js/script.js
@@ -1,36 +1,38 @@
-function operate(num1, num2, operator)
-{
-    let sum;
-    if(operator === '+')
-    {
-        sum = num1 + operator + num2 + '=' + (num1 + num2);
-    }
-    else if(operator === '-')
-    {
-        sum = num1 + operator + num2 + '=' + (num1 - num2);
-    }
-    else if(operator === '*'){
-        sum = num1 + operator + num2 + '=' + (num1 * num2);
-    }
-    else if(operator === '/')
-    {
-        sum = num1 + operator + num2 + '=' + (num1 / num2);
-    }
-    else
-    {
-        return 'Undefined Operator';
-    }
-    return sum;
+const btn = document.querySelectorAll('button');
+const display = document.querySelector('#display');
 
-}
+display.value = 0;
+let numValue1 = '';
+let numValue2 = '';
+let operator = '';
 
-const numButtons = document.querySelectorAll('button');
-
-numButtons.forEach(button => {
-    button.addEventListener('click', buttonClick);
+btn.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if(button.className === 'number'  && operator === '')
+        {
+            numValue1 += e.target.value;
+            display.value = numValue1;
+            debugConsole('numValue1', numValue1);
+        }
+        else if(button.className === 'operator' && operator === '')
+        {
+            operator += e.target.value;
+            display.value = '';
+            display.value = operator;
+            debugConsole('operator', operator);
+        }
+        else if(button.className === 'number'  && operator !== '')
+        {
+            numValue2 += e.target.value;
+            display.value = ''; 
+            display.value = numValue2;
+            debugConsole('numValue2', numValue2);
+        }
+    });
 });
-function buttonClick(e)
-{
-    console.log(e.target.innerText);
-}
 
+
+function debugConsole(valueName,value)
+{
+    console.log(`${valueName} : ${value}`);
+}
